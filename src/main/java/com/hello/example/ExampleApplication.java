231d4d77dc1369e7e9f23892f9725dcd787fa0a2
Kalyan Goud_Nagaram <77835963+kalyan1828@users.noreply.github.com>
2025-02-15 16:07:07 +0530

    Update ExampleApplication.java

--- src/main/java/com/hello/example/ExampleApplication.java
+++ src/main/java/com/hello/example/ExampleApplication.java
@@ -2,8 +2,7 @@ package com.hello.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 
 @SpringBootApplication
@@ -12,9 +11,5 @@ public class ExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
 	}
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(SpringtomcatApplication.class);
-    }
-
+	
 }
