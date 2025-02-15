3e39fa5efe16a6e75eb6c912a53a206a1638cf10
Kalyan Goud_Nagaram <77835963+kalyan1828@users.noreply.github.com>
2025-02-15 16:05:34 +0530

    Update ExampleApplication.java

--- src/main/java/com/hello/example/ExampleApplication.java
+++ src/main/java/com/hello/example/ExampleApplication.java
@@ -2,6 +2,9 @@ package com.hello.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class ExampleApplication {
